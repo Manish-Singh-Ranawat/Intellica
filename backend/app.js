6cca153f8c0b229ef1437fd7963b661b7b936db6
Manish Singh Ranawat <manish27singhranawat@gmail.com>
2025-06-16 17:00:49 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
 import { ENV_VARS } from "./lib/envVars.js";
 import { connectDB } from "./lib/mongodb.js";
 import chatRoutes from "./routes/chat.route.js";
 import { clerkMiddleware } from "@clerk/express";
+import path from "path";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
-
 if (ENV_VARS.NODE_ENV !== "production") {
   app.use(
     cors({
-      origin: ENV_VARS.CLIENT_URL || "http://localhost:5173",
+      origin: ENV_VARS.CLIENT_URL,
       credentials: true,
     })
   );
@@ -25,9 +24,9 @@ app.use("/api/v1/chats", chatRoutes);
 
 if (ENV_VARS.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
